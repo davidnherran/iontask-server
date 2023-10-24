375f3cb06331273e3434e72bd05beb4254cb0f9d
davidnherran <davidnherran@gmail.com>
2023-10-24 07:49:26 -0500

    feat(entities): Add 'user' and 'project' entities to the project

---
+++ src/interfaces/user.interface.ts
@@ -0,0 +1,8 @@
+export interface IUser {
+  firstName: string;
+  lastName: string;
+  email: string;
+  username: string;
+  password: string;
+  role: string;
+}
---
+++ src/users/entities/users.entity.ts
@@ -0,0 +1,24 @@
+import { BaseEntity } from 'src/config/base.entity';
+import { IUser } from 'src/interfaces/user.interface';
+import { Column, Entity } from 'typeorm';
+
+@Entity({ name: 'users' })
+export class UsersEntity extends BaseEntity implements IUser {
+  @Column()
+  firstName: string;
+
+  @Column()
+  lastName: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  username: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  role: string;
+}
