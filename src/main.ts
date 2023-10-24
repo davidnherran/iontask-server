87e765bda31c7984fd873eaaa560e7b28afa1e99
davidnherran <davidnherran@gmail.com>
2023-10-24 07:21:25 -0500

    feat(server): Configure Morgan, CORS, and Prefix

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import * as morgan from 'morgan';
+import { CORS } from './constants';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
+  app.use(morgan('dev'))
+  app.enableCors(CORS)
+  app.setGlobalPrefix('api/v1')
 
   await app.listen(configService.get('PORT'));
+  console.info(`Application is running on ${await app.getUrl()}`)
 }
 bootstrap();
